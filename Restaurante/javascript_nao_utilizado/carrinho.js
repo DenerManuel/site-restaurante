0364bca5ed3ee97d59eb3e98e17483ce02db370b
Dener Emanuel dos Santos <118932133+DenerManuel@users.noreply.github.com>
2026-01-05 15:43:01 -0300

    Subindo meu Restaurante pro Github

---
+++ Restaurante/javascript_nao_utilizado/carrinho.js
@@ -0,0 +1,30 @@
+// const toletinho = {
+//   nome: "Toletinho de Carne",
+//   img: "../img/pagina-pratos/toletinho-de-carne.jpg",
+//   alt: this.nome
+// }
+
+// let tr = document.createElement("tr")
+// tr.className = ("valores")
+
+// let td_item = document.createElement("td")
+// td_item.className = ("item")
+
+// let div = document.createElement("div")
+// div.className = ("item-container")
+
+// let img = document.createElement("img")
+// img.src = (toletinho.img)
+// img.alt = (toletinho.alt)
+
+// let span = document.createElement("span")
+// span.innerText = toletinho.nome
+
+
+// div.appendChild(img)
+// div.appendChild(span)
+// td_item.appendChild(div)
+// tr.appendChild(td_item)
+
+// document.getElementById("container-produto").appendChild(tr)
+// console.log(tr)
---
+++ Restaurante/javascript_nao_utilizado/menu.js
@@ -0,0 +1,482 @@
+// var menu = {
+//   // PRATOS
+
+//   // Linguiça Assada
+//   linguica: {
+//     nome: "Linguiça Assada",
+//     preco: 27.9,
+//     endereco_img: "../img/pagina-pratos/linguica.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Toletinho de Carne
+//   toletinho_de_carne: {
+//     nome: "Toletinho de carne",
+//     preco: 79.9,
+//     endereco_img: "../img/pagina-pratos/linguica.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Cará Tilápia Frita
+//   cara_tilapia_frita: {
+//     nome: "Cará Tilápia Frita",
+//     preco: 37.9,
+//     endereco_img: "../img/pagina-pratos/cara-tilapia-frita.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Bolinho de arroz
+//   bolinho_de_arroz: {
+//     nome: "Bolinho de arroz",
+//     preco: 55.9,
+//     endereco_img: "../img/pagina-pratos/bolinho-de-arroz.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Isca de Carne
+//   isca_de_carne: {
+//     nome: "Isca de carne",
+//     preco: 99.9,
+//     endereco_img: "../img/pagina-pratos/isca-de-carne.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Camarão cozido
+//   camarao_cozido: {
+//     nome: "Camarão cozido",
+//     preco: 69.9,
+//     endereco_img: "../img/pagina-pratos/camarao-cozido.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Filé Mal Passado
+//   file_mal_passado: {
+//     nome: "Filé Mal Passado",
+//     preco: 99.9,
+//     endereco_img: "../img/pagina-pratos/file-mal-passado.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Bisteca Suína Frita
+//   bisteca_suina_frita: {
+//     nome: "Bisteca Suína Frita",
+//     preco: 99.9,
+//     endereco_img: "../img/pagina-pratos/bisteca-frita.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+
+//   // BEBIDAS
+
+//   // Rainbow Mocktail
+//   rainbow_mocktail: {
+//     nome: "Rainbow Mocktail",
+//     preco: 19.9,
+//     endereco_img: "../img/pagina-bebidas/rainbow-mocktail.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Smoothie de Melancia
+//   smoothie_de_melancia: {
+//     nome: "Smoothie de Melancia",
+//     preco: 29.9,
+//     endereco_img: "../img/pagina-bebidas/smoothie-melancia.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Summer Drink
+//   summer_drink: {
+//     nome: "Summer Drink",
+//     preco: 27.9,
+//     endereco_img: "../img/pagina-bebidas/summer-drink.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Suco de Laranja
+//   suco_de_laranja: {
+//     nome: "Suco de Laranja",
+//     preco: 5.9,
+//     endereco_img: "../img/pagina-bebidas/suco-de-laranja.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Água
+//   agua: {
+//     nome: "Água",
+//     preco: 2.5,
+//     endereco_img: "../img/pagina-bebidas/agua.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Chocolate Quente
+//   chocolate_quente: {
+//     nome: "Chocolate Quente",
+//     preco: 15.99,
+//     endereco_img: "../img/pagina-bebidas/chocolate-quente.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Shake de Morango
+//   shake_morango: {
+//     nome: "Shake de Morango",
+//     preco: 12.7,
+//     endereco_img: "../img/pagina-bebidas/chocolate-quente.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Cappuccino
+//   cappuccino: {
+//     nome: "Cappuccino",
+//     preco: 12.99,
+//     endereco_img: "../img/pagina-bebidas/cappuccino.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+
+//   // PETISCOS
+
+//   // Frango Frito
+//   frango_frito: {
+//     nome: "Frango Frito",
+//     preco: 49.9,
+//     endereco_img: "../img/pagina-petiscos/frango-frito.webp",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Batata Doce
+//   batata_doce: {
+//     nome: "Batata Doce",
+//     preco: 29.9,
+//     endereco_img: "../img/pagina-petiscos/batata-doce.webp",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Bolinho de Mandioca
+//   bolinho_de_mandioca: {
+//     nome: "Bolinho de Mandioca",
+//     preco: 30.9,
+//     endereco_img: "../img/pagina-petiscos/bolinho-de-mandioca.webp",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Bolinho de Carne
+//   bolinho_de_carne: {
+//     nome: "Bolinho de Carne",
+//     preco: 50.9,
+//     endereco_img: "../img/pagina-petiscos/bolinho-de-carne.webp",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Batata Frita
+//   batata_frita: {
+//     nome: "Batata Frita",
+//     preco: 30.9,
+//     endereco_img: "../img/pagina-petiscos/batata-frita.jpeg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Tulipa de Frango-Frito
+//   tulipa_de_frango_frito: {
+//     nome: "Tulipa de Frango-Frito",
+//     preco: 69.9,
+//     endereco_img: "../img/pagina-petiscos/tulipa-de-frango-frito.jpeg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Mandioca Frita
+//   mandioca_frita: {
+//     nome: "Mandioca Frita",
+//     preco: 22.9,
+//     endereco_img: "../img/pagina-petiscos/mandioca-frita.webp",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Isca de Peixe
+//   isca_de_peixe: {
+//     nome: "Isca de Peixe",
+//     preco: 45.9,
+//     endereco_img: "../img/pagina-petiscos/iscas-de-peixe.jpeg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+
+//   // SOBREMESAS
+
+//   // Bolo de Cenoura
+//   bolo_de_cenoura: {
+//     nome: "Bolo de Cenoura",
+//     preco: 19.9,
+//     endereco_img: "../img/pagina-sobremesas/bolo-de-cenoura.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Fatia de Torta Doce
+//   fatia_de_torta_doce: {
+//     nome: "Fatia de Torta Doce",
+//     preco: 19.9,
+//     endereco_img: "../img/pagina-sobremesas/fatia-de-torta-doce.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Bolinho de Chocolate
+//   bolinho_de_chocolate: {
+//     nome: "Bolinho de Chocolate",
+//     preco: 19.9,
+//     endereco_img: "../img/pagina-sobremesas/bolinho-de-chocolate.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Sorvete com Calda de Chocolate
+//   sorvete_com_calda_de_chocolate: {
+//     nome: "Sorvete com Calda de Chocolate",
+//     preco: 19.9,
+//     endereco_img: "../img/pagina-sobremesas/sorvete-com-calda-de-chocolate.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Pudim
+//   pudim: {
+//     nome: "Pudim",
+//     preco: 19.9,
+//     endereco_img: "../img/pagina-sobremesas/pudim.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Ovo de Páscoa
+//   ovo_de_pascoa: {
+//     nome: "Ovo de Páscoa",
+//     preco: 19.9,
+//     endereco_img: "../img/pagina-sobremesas/ovo-de-pascoa.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+//   // Fatia de Bolo
+//   fatia_de_bolo: {
+//     nome: "Fatia de Bolo",
+//     preco: 19.9,
+//     endereco_img: "../img/pagina-sobremesas/fatia-de-bolo.jpg",
+//     alt: this.nome,
+//     quantidade: 0,
+//   },
+// };
+
+// const INICIO_PRATOS = "linguica";
+// const INICIO_BEBIDAS = "rainbow_mocktail";
+// const INICIO_PETISCOS = "frango_frito";
+// const INICIO_SOBREMESA = "bolo_de_cenoura";
+// var n = 0;
+
+// function deveIniciarIteracao(chave, item) {
+//   return chave === item;
+// }
+
+// function devePararIteracao(chave, item) {
+//   return chave === item;
+// }
+
+// function criarElementosHtml() {
+//   const div_item = document.createElement("div");
+//   div_item.className = "item";
+
+//   const h2 = document.createElement("h2");
+//   h2.className = "sugestoes";
+
+//   const div_informacoes_item = document.createElement("div");
+//   div_informacoes_item.className = "informacoes-item";
+
+//   const img = document.createElement("img");
+
+//   const p_preco = document.createElement("p");
+//   p_preco.className = "preco-item";
+
+//   const div_qtd_item = document.createElement("div");
+//   div_qtd_item.className = "qtd-item";
+
+//   const btn_diminuir_qtd = document.createElement("button");
+//   btn_diminuir_qtd.textContent = "-";
+
+//   const input = document.createElement("input");
+//   input.type = "number";
+//   input.placeholder = "0";
+
+//   const btn_aumentar_qtd = document.createElement("button");
+//   btn_aumentar_qtd.textContent = "+";
+
+//   const button_carrinho = document.createElement("button");
+//   button_carrinho.className = "adicionar-carrinho-button";
+//   button_carrinho.textContent = "Carrinho";
+
+//   return {
+//     div_item,
+//     h2,
+//     div_informacoes_item,
+//     img,
+//     p_preco,
+//     div_qtd_item,
+//     btn_diminuir_qtd,
+//     input,
+//     btn_aumentar_qtd,
+//     button_carrinho,
+//   };
+//   // section = document.getElementsByClassName("container-item");
+
+//   // div_item = document.createElement("div");
+//   // div_item.className = "item";
+
+//   // h2 = document.createElement("h2");
+//   // h2.className = "sugestoes";
+
+//   // div_informacoes_item = document.createElement("div");
+//   // div_informacoes_item.className = "informacoes-item";
+
+//   // img = document.createElement("img");
+
+//   // p_preco = document.createElement("p");
+//   // p_preco.className = "preco-item";
+
+//   // div_qtd_item = document.createElement("div");
+//   // div_qtd_item.className = "qtd-item";
+
+//   // btn_diminuir_qtd = document.createElement("button");
+//   // btn_diminuir_qtd.value = "-";
+
+//   // input = document.createElement("input");
+//   // input.type = "number";
+
+//   // btn_aumentar_qtd = document.createElement("button");
+//   // btn_aumentar_qtd.value = "+";
+
+//   // button_carrinho = document.createElement("button");
+//   // button_carrinho.className = "adicionar-carrinho-button";
+// }
+// let section = document.getElementById("container-item");
+
+// for (let chaveDoItem in menu) {
+//   let estaRodando = true;
+
+//   // if (deveIniciarIteracao(chaveDoItem, INICIO_PRATOS)) {
+//   //   estaRodando = true;
+//   // }
+
+//   if (devePararIteracao(chaveDoItem, INICIO_BEBIDAS)) {
+//     estaRodando = false;
+//     console.log(n);
+//   }
+
+//   if (estaRodando) {
+//     n++;
+//     console.log("Testando");
+//     const {
+//       div_item,
+//       h2,
+//       div_informacoes_item,
+//       img,
+//       p_preco,
+//       div_qtd_item,
+//       btn_diminuir_qtd,
+//       input,
+//       btn_aumentar_qtd,
+//       button_carrinho,
+//     } = criarElementosHtml();
+
+//     h2.textContent = menu[chaveDoItem].nome;
+//     img.src = menu[chaveDoItem].endereco_img;
+//     img.alt = menu[chaveDoItem].alt;
+//     p_preco.textContent = `R$ ${menu[chaveDoItem].preco.toFixed(2)}`;
+//     btn_diminuir_qtd.setAttribute(
+//       "onclick",
+//       `diminuirQtd('${chaveDoItem}')`
+//     );
+//     btn_aumentar_qtd.setAttribute(
+//       "onclick",
+//       `aumentarQtd('${chaveDoItem}')`
+//     );
+//     button_carrinho.setAttribute(
+//       "onclick",
+//       `carrinho('${chaveDoItem}')`
+//     );
+
+//     div_qtd_item.appendChild(btn_diminuir_qtd);
+//     div_qtd_item.appendChild(input);
+//     div_qtd_item.appendChild(btn_aumentar_qtd);
+//     div_informacoes_item.appendChild(img);
+//     div_informacoes_item.appendChild(p_preco);
+//     div_informacoes_item.appendChild(div_qtd_item);
+//     div_item.appendChild(h2);
+//     div_item.appendChild(div_informacoes_item);
+//     div_informacoes_item.appendChild(button_carrinho);
+
+//     section.appendChild(div_item);
+//     // h2.textContent = menu[chaveDoItem].nome;
+//     // div_item.appendChild(h2);
+//     // img.src = menu[chaveDoItem].endereco_img;
+//     // img.alt = menu[chaveDoItem].alt;
+//     // p_preco.textContent = `R$ ${menu[chaveDoItem].preco}`;
+//     // div_informacoes_item.appendChild(img);
+//     // div_informacoes_item.appendChild(p_preco);
+//     // btn_diminuir_qtd.onclick = function () {
+//     //   diminuirQtd(chaveDoItem);
+//     // };
+//     // btn_aumentar_qtd.onclick = function () {
+//     //   aumentarQtd(chaveDoItem);
+//     // };
+//     // div_qtd_item.appendChild(btn_diminuir_qtd);
+//     // div_qtd_item.appendChild(input);
+//     // div_qtd_item.appendChild(btn_aumentar_qtd);
+//     // div_informacoes_item.appendChild(div_qtd_item);
+//     // button_carrinho.onclick = `carrinho(${chaveDoItem})`;
+//     // div_item.appendChild(div_informacoes_item);
+//     // div_item.appendChild(button_carrinho);
+
+//     // section.appendChild(div_item);
+//   } else {
+//     break;
+//   }
+// }
+
+// // var carrinho = {};
+
+// // if (localStorage.getItem("carrinho")) {
+// //   carrinho = JSON.parse(localStorage.getItem("carrinho"));
+// // }
+
+// // function add(produto, idInput) {
+// //   if (carrinho[produto.nome]) {
+// //     carrinho[produto.nome].quantidade++;
+// //   } else {
+// //     carrinho[produto.nome] = {
+// //       quantidade: 1,
+// //       nome: produto.nome,
+// //       preco: produto.preco,
+// //     };
+// //   }
+// //   console.log(carrinho);
+// //   document.getElementById(idInput).value = carrinho[produto.nome].quantidade;
+
+// //   localStorage.setItem("carrinho", JSON.stringify(carrinho));
+// // }
+
+// // window.addEventListener("load", function () {
+// //   this.document
+// //     .querySelectorAll("input")
+// //     .forEach(
+// //       (input) => (input.value = carrinho[input.dataset["produto"]].quantidade)
+// //     );
+// // });
+
+// // function limparCarrinho() {
+// //   localStorage.removeItem("carrinho");
+// // }
+
+// // const pratos = 0
+// // const bebidas = 1
+// // const petiscos = 2
+// // const sobremesas = 3
+
+// // var menu = [
+// //   [
+// //     linguica: {
+
+// //     }
+// //   ]
+// // ]
